e9c4a06ca1fff87cfd548ac4447f22a6d75c2c96
d-yv <yuri.dubrovin@gmail.com>
2025-04-07 22:08:12 +0300

    add structure

--- js/task-3.js
+++ js/task-3.js
@@ -1,15 +1,9 @@
-function filterArray(numbers, value) {
-    let newArray = [];
-    for (let number of numbers) {
-        if (number > value) {
-            newArray.push(number);
-        }
-    }
-    return newArray;
-}
-
-console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
-console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
-console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
-console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
-console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+//test
+const builder = new StringBuilder('.');
+console.log(builder.getValue()); // "."
+builder.padStart('^');
+console.log(builder.getValue()); // "^."
+builder.padEnd('^');
+console.log(builder.getValue()); // "^.^"
+builder.padBoth('=');
+console.log(builder.getValue()); // "=^.^="
