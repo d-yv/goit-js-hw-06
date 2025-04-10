58875a3e7359ba7871eff2357effeea70ea0d0b8
d-yv <yuri.dubrovin@gmail.com>
2025-04-10 22:36:33 +0300

    add task-3

--- js/task-3.js
+++ js/task-3.js
@@ -1,3 +1,21 @@
+class StringBuilder {
+  constructor(initialValue) {
+    this._value = initialValue;
+  }
+  getValue() {
+    return this._value;
+  }
+  padEnd(str) {
+    return (this._value += str);
+  }
+  padStart(str) {
+    return (this._value = str + this._value);
+  }
+  padBoth(str) {
+    return (this._value = str + this._value + str);
+  }
+}
+
 //test
 const builder = new StringBuilder('.');
 console.log(builder.getValue()); // "."
