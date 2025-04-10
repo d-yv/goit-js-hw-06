bb6a7d1908ae797c5f42483bf99899816db88c28
d-yv <yuri.dubrovin@gmail.com>
2025-04-10 18:32:34 +0300

    add task2

--- js/task-2.js
+++ js/task-2.js
@@ -1,3 +1,21 @@
+class Storage {
+  #items;
+  constructor(items) {
+    this.#items = items;
+  }
+  getItems() {
+    return this.#items;
+  }
+  addItem(newItem) {
+    this.#items.push(newItem);
+  }
+  removeItem(itemToRemove) {
+    if (this.#items.includes(itemToRemove)) {
+      this.#items.splice(this.#items.indexOf(itemToRemove), 1);
+    }
+  }
+}
+
 //test
 const storage = new Storage(['Nanitoids', 'Prolonger', 'Antigravitator']);
 console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
