918ebdf6229b6b80eeee9778b77f970c81fb73a7
d-yv <yuri.dubrovin@gmail.com>
2025-04-10 22:44:59 +0300

    fix errors

--- js/task-3.js
+++ js/task-3.js
@@ -1,18 +1,19 @@
 class StringBuilder {
+  #value;
   constructor(initialValue) {
-    this._value = initialValue;
+    this.#value = initialValue;
   }
   getValue() {
-    return this._value;
+    return this.#value;
   }
   padEnd(str) {
-    return (this._value += str);
+    return (this.#value += str);
   }
   padStart(str) {
-    return (this._value = str + this._value);
+    return (this.#value = str + this.#value);
   }
   padBoth(str) {
-    return (this._value = str + this._value + str);
+    return (this.#value = str + this.#value + str);
   }
 }
 
